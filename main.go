2a06b1e2ee01c41c3ea705dcf57720f1584af36c
a-was <adam.wasilewski@unix.com.pl>
2025-05-23 12:44:47 +0200

    Hello Go!

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.String(http.StatusOK, "pong")
+	})
+
+	// Listen and Server in 0.0.0.0:8080
+	r.Run(":8080")
+}
